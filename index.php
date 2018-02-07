e3bb6d796958b1b38ce9ed955b52e577e15bd9a0
William Belke III <200125014@fvtc.edu>
2018-02-07 17:54:50 -0600

    Message

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTP=8">
+    <title>PHP Class - William Belke</title>
+    <link rel="stylesheet" type="text/css" href="CSS/MyStyles.css"/>
+</head>
+
+<body>
+<div class="page-wrap"></div>
+<header class="site-header">
+    <h1>William Belke PHP</h1>
+</header>
+<div class="flex-box">
+    <nav class="main=nav">
+        <ul>
+            <li class="menu-item"><a href="/">Home</a></li>
+            <li class="menu-item"><a href="#">Loops</a></li>
+            <li class="menu-item"><a href="#">Strings</a></li>
+        </ul>
+    </nav>
+
+    <section class="main-content">
+        <div class="img">Picture</div>
+        <h2>Welcome!</h2>
+        <div class="content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia sapien ac nisi aliquet, semper.
+        </div>
+
+
+        <aside class="right-sidebar">Sidebar</aside>
+</div> <!-- End flex box-->
+<footer>
+    <div>Learn PHP <sup>&copy;</sup>2018</div>
+</footer>
+
+
+</body>
+</html>
